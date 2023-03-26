6232d95c914a5b817ba092d8219feea267be063d
Renjithkumar993 <121647888+Renjithkumar993@users.noreply.github.com>
2023-03-26 20:16:47 -0300

    added delete

--- Develop/server.js
+++ Develop/server.js
@@ -1,14 +1,16 @@
+const { json } = require('express');
 const express = require('express');
 const PORT = 3001;
 const app = express();
 const path = require('path');
 const notes = require("./db/db.json")
+const fs = require("fs")
 
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
+app.use(express.static('public'));
 
 
 app.get('/', (req, res) => {
@@ -18,11 +20,71 @@ app.get('/', (req, res) => {
 
 app.get('/notes' ,(req,res) =>{
     res.sendFile(path.join(__dirname, './public/notes.html'));
-    return notes
+})
+
+app.get("/api/notes" , (req,res) =>{
+ res.json(notes)
 })
 
 
 
+app.get("/api/notes/:notes_id", (req, res) => {
+
+  const requestedID = parseInt(req.params.notes_id);
+      for (let i = 0; i < notes.length; i++) {
+        if (requestedID === notes[i].id) {
+          return res.json(notes[i]);
+        }
+      }
+      return res.status(404).send("Note not found");
+    }
+  );
+
+
+ 
+app.post("/api/notes", (req, res) => {
+  const { title, text } = req.body;
+
+  if (title && text) {
+    fs.readFile("./db/db.json", (err, data) => {
+      if (err) {
+        console.error(err);
+      } else {
+        const notes = JSON.parse(data);
+        const newNote = { title, text, id: notes.length + 1 };
+      
+        notes.push(newNote);
+        fs.writeFile("./db/db.json", JSON.stringify(notes), (err) => {
+          if (err) {
+            console.error(err);
+          } else {
+            res.json(newNote);
+          }
+        });
+      }
+    });
+  } else {
+    res.status(400).send("Invalid note format");
+  }
+});
+
+
+app.delete("/api/notes/:notes_id",(req,res) => {
+  
+  const deleteID  = parseInt(req.params.notes_id);
+  for (let i = 0; i < notes.length; i++) {
+    if (deleteID === notes[i].id) {
+     res.json(notes.splice(i,1))
+    fs.writeFile("./db/db.json", JSON.stringify(notes), (err) => {
+      if (err) {
+        console.error(err);
+      } 
+    }
+)}
+}
+
+
+})
 
 
 
