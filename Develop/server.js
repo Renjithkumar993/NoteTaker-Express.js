7a23c2206c9522ee140569df46fc85ec4c1194f3
Renjithkumar993 <121647888+Renjithkumar993@users.noreply.github.com>
2023-03-26 21:45:16 -0300

    n

--- Develop/server.js
+++ Develop/server.js
@@ -7,6 +7,7 @@ const notes = require("./db/db.json")
 const fs = require("fs")
 
 
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -14,16 +15,16 @@ app.use(express.static('public'));
 
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
-  });
-  
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
 
-app.get('/notes' ,(req,res) =>{
-    res.sendFile(path.join(__dirname, './public/notes.html'));
+
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/notes.html'));
 })
 
-app.get("/api/notes" , (req,res) =>{
- res.json(notes)
+app.get("/api/notes", (req, res) => {
+  res.json(notes)
 })
 
 
@@ -31,57 +32,50 @@ app.get("/api/notes" , (req,res) =>{
 app.get("/api/notes/:notes_id", (req, res) => {
 
   const requestedID = parseInt(req.params.notes_id);
-      for (let i = 0; i < notes.length; i++) {
-        if (requestedID === notes[i].id) {
-          return res.json(notes[i]);
-        }
-      }
-      return res.status(404).send("Note not found");
+  for (let i = 0; i < notes.length; i++) {
+    if (requestedID === notes[i].id) {
+      return res.json(notes[i]);
     }
-  );
+  }
+  return res.status(404).send("Note not found");
+}
+);
 
 
- 
+
 app.post("/api/notes", (req, res) => {
   const { title, text } = req.body;
 
   if (title && text) {
-    fs.readFile("./db/db.json", (err, data) => {
+    const newNote = { title, text, id: notes.length + 1 };
+
+    notes.push(newNote);
+
+    fs.writeFile("./db/db.json", JSON.stringify(notes), (err) => {
       if (err) {
         console.error(err);
       } else {
-        const notes = JSON.parse(data);
-        const newNote = { title, text, id: notes.length + 1 };
-      
-        notes.push(newNote);
-        fs.writeFile("./db/db.json", JSON.stringify(notes), (err) => {
-          if (err) {
-            console.error(err);
-          } else {
-            res.json(newNote);
-          }
-        });
+        res.json(notes);
       }
     });
-  } else {
-    res.status(400).send("Invalid note format");
   }
 });
 
 
-app.delete("/api/notes/:notes_id",(req,res) => {
-  
-  const deleteID  = parseInt(req.params.notes_id);
+app.delete("/api/notes/:notes_id", (req, res) => {
+
+  const deleteID = parseInt(req.params.notes_id);
   for (let i = 0; i < notes.length; i++) {
     if (deleteID === notes[i].id) {
-     res.json(notes.splice(i,1))
-    fs.writeFile("./db/db.json", JSON.stringify(notes), (err) => {
-      if (err) {
-        console.error(err);
-      } 
+      res.json(notes.splice(i, 1))
+      fs.writeFile("./db/db.json", JSON.stringify(notes), (err) => {
+        if (err) {
+          console.error(err);
+        }
+      }
+      )
     }
-)}
-}
+  }
 
 
 })
@@ -100,5 +94,5 @@ app.delete("/api/notes/:notes_id",(req,res) => {
 
 
 app.listen(PORT, () =>
-console.log(`Serving static asset routes on port http://127.0.0.1:${PORT}/ !`)
+  console.log(`Serving static asset routes on port http://127.0.0.1:${PORT}/ !`)
 );
